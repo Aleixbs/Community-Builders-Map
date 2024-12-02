bf921643ace321195d4c996e14582fb6a0efdac0
Álvaro Gutiérrez <124072319+alvarogtrzcliment@users.noreply.github.com>
2024-12-02 18:14:17 +0100

    add pan/rotate movement, communityBuilders FeatureLayer

--- main.js
+++ main.js
@@ -1,36 +1,55 @@
-require([ "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/symbols/SimpleFillSymbol", "esri/layers/GraphicsLayer","esri/Graphic" ], (Map, MapView, FeatureLayer, SimpleFillSymbol, GraphicsLayer, Graphic) => {
-  
-  const tamanoPantralla = [window.screen.width, window.screen.height]
-  
+require([
+  'esri/Map',
+  'esri/views/MapView',
+  'esri/layers/FeatureLayer',
+  'esri/symbols/SimpleFillSymbol',
+  'esri/layers/GraphicsLayer',
+  'esri/Graphic',
+  'esri/core/reactiveUtils'
+], (
+  Map,
+  MapView,
+  FeatureLayer,
+  SimpleFillSymbol,
+  GraphicsLayer,
+  Graphic,
+  reactiveUtils
+) => {
+  body = document.getElementsByTagName('body')
+  const tamanoPantalla = [body[0].clientWidth, body[0].clientWidth]
+  console.log(tamanoPantalla)
+  let centroides = []
+  let centroidesTransformados
+
   const mapa = new Map({
-    basemap: "dark-gray-vector"
-  });
+    basemap: 'dark-gray-vector'
+  })
 
   const vista = new MapView({
-    container: "mapaDiv",
+    container: 'mapaDiv',
     map: mapa,
     zoom: 5,
     center: [-4, 40]
-  });
+  })
 
   // Provincias FL Renderer
 
   const simpleFillSymbol = new SimpleFillSymbol({
     outline: {
-      cap: "round",
-      color: [0,255,204,0.3],
-      join: "round",
+      cap: 'round',
+      color: [0, 255, 204, 0.3],
+      join: 'round',
       miterLimit: 1,
-      style: "solid",
+      style: 'solid',
       width: 0.5
     },
-    style: "solid"
-  });
+    style: 'solid'
+  })
 
   const provinciasRenderer = {
-    type: "simple",  
+    type: 'simple',
     symbol: simpleFillSymbol
-  };
+  }
 
   const provinciasFl = new FeatureLayer({
     portalItem: {
@@ -38,77 +57,73 @@ require([ "esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/sy
     },
     renderer: provinciasRenderer,
     effect: 'bloom(1, 1px, 0.0)'
-    
   })
 
-  mapa.add(provinciasFl)
+  const communityBuildersFl = new FeatureLayer({
+    portalItem: {
+      id: '231c32bc0150403a973c4289ee8bba60'
+    }
+  })
 
-  let centroides = []
-  let centroidesTransformados
+  mapa.addMany([communityBuildersFl, provinciasFl])
 
   // Cuando se carga esta capa se hace la llamada
 
   vista.whenLayerView(provinciasFl).then(() => {
-
-    provinciasFl.queryFeatures().then(results => {
+    provinciasFl.queryFeatures().then((results) => {
       const resultadoFeatures = results.features
-
-      const capaGrafica = new GraphicsLayer()
-
-      resultadoFeatures.map((entidad)=>{
+      resultadoFeatures.map((entidad) => {
         centroides.push(entidad.geometry.extent.center)
       })
-
-      const centroidesPintar = resultadoFeatures.map((entidad)=>{
-        const centroidePintar = new Graphic({
-          geometry: entidad.geometry.extent.center
-        })
-        return centroidePintar
-      })
-
-      capaGrafica.addMany(centroidesPintar)
-      mapa.add(capaGrafica)
-
     })
-  });
+  })
 
-  
-
-  vista.on('mouse-wheel',(evento)=>{
-    if(vista.zoom >= 8){
-      console.log(evento)
-
-      const elementos = document.querySelectorAll('.popup-centroide')
-      elementos.forEach((elemento)=>{
-        elemento.remove()
-      })
-
-
-
-      setTimeout(()=>{
+  reactiveUtils.watch(
+    () => vista.extent,
+    () => {
+      if (vista.zoom > 5) {
+        const elementos = document.querySelectorAll('.popup-centroide')
+        elementos.forEach((elemento) => {
+          elemento.remove()
+        })
 
         centroidesTransformados = centroides.map((centroide) => {
           return vista.toScreen(centroide)
         })
-  
-        const centrosFiltrados = centroidesTransformados.filter((centroide)=>{
-          if(( 0 < centroide.x && centroide.x < tamanoPantralla[0]) && ( 0 < centroide.y && centroide.y < tamanoPantralla[1])){
+
+        centroidesTransformados.map((centro) => {
+          const divPopup = document.createElement('div')
+          divPopup.className = 'popup-centroide'
+          divPopup.style.left = `${centro.x - 100 / 2}px`
+          divPopup.style.top = `${centro.y - 75 / 2}px`
+          const parentNode = document.getElementById('mapaDiv')
+          document.body.insertBefore(divPopup, parentNode)
+        })
+
+        const centrosFiltrados = centroidesTransformados.filter((centroide) => {
+          if (
+            0 < centroide.x &&
+            centroide.x < tamanoPantalla[0] &&
+            0 < centroide.y &&
+            centroide.y < tamanoPantalla[1]
+          ) {
             return true
-          }else{
+          } else {
             return false
           }
         })
-
-        centrosFiltrados.map((centro)=>{
-          console.log(centro.x)
-          const divPopup = document.createElement('div')
-          divPopup.className = 'popup-centroide'
-          divPopup.style.left = `${centro.x-25}px`
-          divPopup.style.top = `${centro.y-25}px`
-          const parentNode = document.getElementById('mapaDiv')
-          document.body.insertBefore(divPopup,parentNode)
+      } else {
+        const elementos = document.querySelectorAll('.popup-centroide')
+        elementos.forEach((elemento) => {
+          elemento.remove()
         })
-      },'200')
+      }
+
+      centroidesTransformados = centroides.map((centroide) => {
+        return vista.toScreen(centroide)
+      })
     }
-  })
-});
+  )
+
+  // Gestión del movimiento del mapa
+})
